bf2c45291bd768f8dd3316e976e8c3602ef485fd
Nhan Nguyen <thanhnhan.uit90@gmail.com>
2021-07-27 18:43:59 -0500

    Change cache config

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os
 class Config(object):
     ENV = os.getenv('FLASK_ENV')
     if ENV == 'development':
-        CACHE_TYPE = 'redis'
+        CACHE_TYPE = 'RedisCache'
         CACHE_REDIS_HOST = 'localhost'
         CACHE_REDIS_PORT = 6379
         CACHE_REDIS_DB = 0
         CACHE_REDIS_URL = 'redis://localhost:6379/0'
         CACHE_DEFAULT_TIMEOUT = 3600
     else:
-        CACHE_TYPE = 'redis'
+        CACHE_TYPE = 'RedisCache'
         CACHE_REDIS_HOST = 'redis'
         CACHE_REDIS_PORT = 6379
         CACHE_REDIS_DB = 0
