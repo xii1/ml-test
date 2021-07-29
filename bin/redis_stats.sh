39730e9c092f6ca83d6deb92ebf74637e5f327da
Nhan Nguyen <thanhnhan.uit90@gmail.com>
2021-07-29 14:14:52 -0500

    Implement movies recommender for collaborative filtering use SVD

--- bin/redis_stats.sh
+++ bin/redis_stats.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker exec -it redis redis-cli info stats | grep -E '^keyspace|expired_keys|evicted_keys'
+REDIS_DOCKER_NAME=redis
+if [ $# -gt 0 ]
+  then
+    REDIS_DOCKER_NAME=$1
+fi
+
+docker exec -it $REDIS_DOCKER_NAME redis-cli info stats | grep -E '^keyspace|expired_keys|evicted_keys'
