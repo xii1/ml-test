b392f0f35687e72a0046e2c57b2d18030123f3d9
Nhan Nguyen <thanhnhan.uit90@gmail.com>
2021-08-07 10:49:46 -0500

    Training model for handwritten mnist

--- api/classifier.py
+++ api/classifier.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 
-from ml.sample_classification import predict_dog_cat
+from ml.dogcat_classification import predict_dog_cat
+from ml.handwritten_mnist import predict_handwritten
 from utils import convert_image_to_base64
 
 classifier = Blueprint('classifier', __name__)
@@ -14,3 +15,13 @@ def recognize_dog_cat():
         result = predict_dog_cat(uploaded_file)
         return render_template('result.html', img=img, message=result)
     return render_template('upload.html', url='dogcat')
+
+
+@classifier.route('/handwritten', methods=['GET', 'POST'])
+def recognize_handwritten():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        img = convert_image_to_base64(uploaded_file)
+        result = predict_handwritten(uploaded_file)
+        return render_template('result.html', img=img, message=result)
+    return render_template('upload.html', url='handwritten')
